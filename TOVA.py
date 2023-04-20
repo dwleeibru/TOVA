0b5e807daf1f8e675d0b54aeef661aed5f31ede5
dwleeibru <dwlee_ibru@gmail.com>
2023-04-19 22:41:45 -0400

    cannot output excel

--- TOVA.py
+++ TOVA.py
@@ -85,15 +85,15 @@ st.dataframe(df1)
 
 out_path=penguin_file.name[0:-6] + '_Zscore.xlsx'
 
-# writer = pd.ExcelWriter(out_path,engine='xlsxwriter')
-# df1.to_excel(writer,sheet_name="Sheet1",index=True, header=False)
+writer = pd.ExcelWriter(out_path)
+df1.to_excel(writer,sheet_name="Sheet1",index=True, header=False)
 
-# workbook = writer.book
-# worksheet = writer.sheets['Sheet1']
-# worksheet.autofit()
+workbook = writer.book
+worksheet = writer.sheets['Sheet1']
+worksheet.autofit()
 
-# writer.close()
+writer.close()
 
-with pd.ExcelWriter(out_path,engine='xlsxwriter') as writer:
-	df1.to_excel(writer,sheet_name="Sheet1",index=True, header=False)
-	writer.save()
+# with pd.ExcelWriter(out_path) as writer:
+# 	df1.to_excel(writer,sheet_name="Sheet1",index=True, header=False)
+# 	writer.save()
